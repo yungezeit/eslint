f26be991be505fd3bd1defc031aaf2221c094be6
Charles Gruenais <charles.gruenais@gmail.com>
2024-08-07 22:55:26 +0200

    fix: add typescript-eslint parser for vue files

---
+++ .changeset/loud-boxes-end.md
@@ -0,0 +1,9 @@
+---
+"@yungezeit/eslint-vue": patch
+"@yungezeit/eslint-base": patch
+"@yungezeit/eslint-cypress": patch
+"@yungezeit/eslint-storybook": patch
+"@yungezeit/eslint-typescript": patch
+---
+
+Added typescript-eslint parser for vue files
--- presets/vue/index.js
+++ presets/vue/index.js
@@ -1,4 +1,15 @@
 import typescriptPreset from '@yungezeit/eslint-typescript';
 import vuePreset from './features/vue.js';
 
-export default [...typescriptPreset, ...vuePreset];
+export default [
+  ...typescriptPreset,
+  ...vuePreset,
+  {
+    files: ['*.vue', '**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+  },
+];
