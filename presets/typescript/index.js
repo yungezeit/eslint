fd4713ef73dfe13bf9c8a4805d5f139dff7e4953
Charles Gruenais <charles.gruenais@gmail.com>
2024-08-07 22:08:47 +0200

    fix: path to test files config in ts preset

--- presets/typescript/index.js
+++ presets/typescript/index.js
@@ -1,5 +1,5 @@
 import basePreset from '@yungezeit/eslint-base';
 import tsPreset from './features/ts.js';
-import tsTestsPreset from './features/ts.js';
+import { testFiles } from './features/test-files.js';
 
-export default [...basePreset, ...tsPreset, tsTestsPreset];
+export default [...basePreset, ...tsPreset, testFiles];
